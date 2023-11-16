1ec17502eab98c8961b9bcb4063b900fd2a7e561
Sebastian Camilo Vanegas Ayala <scva9708@gmail.com>
2023-11-15 19:09:13 -0500

    Update EjemploGitHub.java

--- src/main/java/full/ejemplogithub/EjemploGitHub.java
+++ src/main/java/full/ejemplogithub/EjemploGitHub.java
@@ -11,6 +11,6 @@ package full.ejemplogithub;
 public class EjemploGitHub {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Mundo");
     }
 }
