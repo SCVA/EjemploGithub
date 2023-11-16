9f61524aab11c0ac51f42049555d0647fa29bd0a
Sebastian Camilo Vanegas Ayala <scva9708@gmail.com>
2023-11-15 19:01:01 -0500

    Creacion nuevo proyecto

---
+++ src/main/java/full/ejemplogithub/EjemploGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package full.ejemplogithub;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class EjemploGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
